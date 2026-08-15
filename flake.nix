fe670bf32c45a7249c5dfba3f14da2fa7d7d4011
RJ <rybertjen@gmail.com>
2026-08-15 02:52:17 -0700

    build: add named Nix quality gates

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
         "aarch64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      packageVersion = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
     in
     {
       packages = forAllSystems (
@@ -23,7 +24,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "git-autocommit";
-            version = "0.1.0";
+            version = packageVersion;
             src = self;
 
             cargoLock.lockFile = ./Cargo.lock;
@@ -51,9 +52,89 @@
         };
       });
 
-      checks = forAllSystems (system: {
-        default = self.packages.${system}.default;
-      });
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          package = self.packages.${system}.default;
+          mkCargoCheck =
+            {
+              name,
+              command,
+              extraNativeBuildInputs ? [ ],
+            }:
+            package.overrideAttrs (old: {
+              pname = "git-autocommit-check-${name}";
+              nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ extraNativeBuildInputs;
+              doCheck = false;
+              buildPhase = ''
+                runHook preBuild
+                cargo ${command}
+                runHook postBuild
+              '';
+              installPhase = ''
+                runHook preInstall
+                mkdir -p "$out"
+                touch "$out/passed"
+                runHook postInstall
+              '';
+              postInstall = "";
+            });
+          formatCheck = mkCargoCheck {
+            name = "format";
+            command = "format-check";
+            extraNativeBuildInputs = [ pkgs.rustfmt ];
+          };
+          staticAnalysis = mkCargoCheck {
+            name = "static-analysis";
+            command = "static-analysis";
+            extraNativeBuildInputs = [ pkgs.clippy ];
+          };
+          unitTests = mkCargoCheck {
+            name = "unit";
+            command = "test-unit";
+          };
+          propertyTests = mkCargoCheck {
+            name = "property";
+            command = "test-property";
+          };
+          integrationTests = mkCargoCheck {
+            name = "integration";
+            command = "test-integration";
+          };
+          e2eTests = mkCargoCheck {
+            name = "e2e";
+            command = "test-e2e";
+          };
+          releaseBuild = mkCargoCheck {
+            name = "build-release";
+            command = "build-release";
+          };
+          aggregate = pkgs.runCommand "git-autocommit-quality-gates-${packageVersion}" { } ''
+            test -e ${formatCheck}/passed
+            test -e ${staticAnalysis}/passed
+            test -e ${unitTests}/passed
+            test -e ${propertyTests}/passed
+            test -e ${integrationTests}/passed
+            test -e ${e2eTests}/passed
+            test -e ${releaseBuild}/passed
+            test -x ${package}/bin/git-autocommit
+            mkdir -p "$out"
+            touch "$out/passed"
+          '';
+        in
+        {
+          default = aggregate;
+          format = formatCheck;
+          static-analysis = staticAnalysis;
+          unit = unitTests;
+          property = propertyTests;
+          integration = integrationTests;
+          e2e = e2eTests;
+          build-release = releaseBuild;
+          package = package;
+        }
+      );
 
       devShells = forAllSystems (
         system:
@@ -78,6 +159,7 @@
               echo "  cargo format-check"
               echo "  cargo static-analysis"
               echo "  cargo test-unit"
+              echo "  cargo test-property"
               echo "  cargo test-integration"
               echo "  cargo test-e2e"
               echo "  cargo build-release"
